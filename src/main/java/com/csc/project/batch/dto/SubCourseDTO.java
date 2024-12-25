8363fafd5c79ef5885c7cc7c58824eedc1db18d4
SenthilKumar <104403967+Senthil28396@users.noreply.github.com>
2024-12-25 21:58:31 +0530

    subcourse changes (#9)

--- src/main/java/com/csc/project/batch/dto/SubCourseDTO.java
+++ src/main/java/com/csc/project/batch/dto/SubCourseDTO.java
@@ -3,9 +3,11 @@ package com.csc.project.batch.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SubCourseDTO {
 	private Long id;
 	private String name;
