4625186a9b0962cb555df60e1b9c885405a4d912
Priyaram <48385836+priyaram2021@users.noreply.github.com>
2025-01-03 19:20:56 +0530

    delete try in jwtservice (#17)
    * student module added
    
    * configure batch and course module
    
    * configure batch and course module
    
    * undo command lines
    
    * undo the comment lines
    
    * undo comment lines
    
    * undo comment line in resourceNotFoundException
    
    * resolve bugs in render
    
    * remove try in jwt service

--- src/main/java/com/csc/project/security/JwtService.java
+++ src/main/java/com/csc/project/security/JwtService.java
@@ -45,11 +45,6 @@ public class JwtService {
 
 			Jwts.parserBuilder().setSigningKey(SECRET_KEY) // Use the same constant secret key to verify the signature
 					.build().parseClaimsJws(token);
-	public Boolean validateToken(String token, String email) {
-		try {
-
-			Jwts.parserBuilder().setSigningKey(SECRET_KEY) 
-					.build().parseClaimsJws(token);
 
 			return true;
 		} catch (JwtException e) {
