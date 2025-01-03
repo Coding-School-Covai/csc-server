d6c54ff006593ca745c123467f1eb6372cc04d8b
priyaram2021 <priyaram2021@gmail.com>
2025-01-03 18:02:35 +0530

    remove try in jwt service

--- src/main/java/com/csc/project/security/JwtService.java
+++ src/main/java/com/csc/project/security/JwtService.java
@@ -43,11 +43,6 @@ public class JwtService {
 	public Boolean validateToken(String token, String email) {
 		try {
 
-			Jwts.parserBuilder().setSigningKey(SECRET_KEY) // Use the same constant secret key to verify the signature
-					.build().parseClaimsJws(token);
-	public Boolean validateToken(String token, String email) {
-		try {
-
 			Jwts.parserBuilder().setSigningKey(SECRET_KEY) 
 					.build().parseClaimsJws(token);
 
